2894e3895550ab1560b80d4a97e8bd7b5b0887fa
yonimor88 <yonimor88@gmail.com>
2020-05-14 20:09:18 +0300

    Updates

---
+++ precache-manifest.9eefebc0758b3fa0268576f95a0a0e40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc2deff365e64c3c1d7fc2b3b530675",
+    "url": "/game-of-war-rehersal-test/public/index.html"
+  },
+  {
+    "revision": "784ea45ec6960f8eb4a2",
+    "url": "/game-of-war-rehersal-test/public/static/css/main.a8870a49.chunk.css"
+  },
+  {
+    "revision": "81f4287b9b16fe830869",
+    "url": "/game-of-war-rehersal-test/public/static/js/2.32dca813.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/game-of-war-rehersal-test/public/static/js/2.32dca813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "784ea45ec6960f8eb4a2",
+    "url": "/game-of-war-rehersal-test/public/static/js/main.d95315bb.chunk.js"
+  },
+  {
+    "revision": "a7f2d2c16f524c5df38a",
+    "url": "/game-of-war-rehersal-test/public/static/js/runtime-main.93d0a222.js"
+  }
+]);
